bf4a972c367ce2fb968e11abda82c5a88e375117
Dariusz Lenartowicz <dariusz.lenartowicz@gmail.com>
2025-03-23 14:57:54 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -254,42 +254,11 @@ then
     git tag -a "$new" -m "$tag_message" || exit 1
 else
     echo "EVENT: creating local tag $new"
-    git tag -f "$new" || exit 1
+    # git tag -f "$new" || exit 1
+    git tag -a "$new" -m "$new" || exit 1
 fi
 
 echo "EVENT: pushing tag $new to origin"
 
-if $git_api_tagging
-then
-    # use git api to push
-    dt=$(date '+%Y-%m-%dT%H:%M:%SZ')
-    full_name=$GITHUB_REPOSITORY
-    git_refs_url=$(jq .repository.git_refs_url "$GITHUB_EVENT_PATH" | tr -d '"' | sed 's/{\/sha}//g')
-
-    echo "$dt: **pushing tag $new to repo $full_name"
-
-    git_refs_response=$(
-    curl -s -X POST "$git_refs_url" \
-    -H "Authorization: token $GITHUB_TOKEN" \
-    -d @- << EOF
-{
-    "ref": "refs/tags/$new",
-    "sha": "$commit"
-}
-EOF
-)
-
-    git_ref_posted=$( echo "${git_refs_response}" | jq .ref | tr -d '"' )
-
-    echo "::debug::${git_refs_response}"
-    if [ "${git_ref_posted}" = "refs/tags/${new}" ]
-    then
-        exit 0
-    else
-        echo "::error::Tag was not created properly."
-        exit 1
-    fi
-else
-    # use git cli to push
-    git push -f origin "$new" || exit 1
-fi
+# use git cli to push
+git push -f origin "$new" || exit 1
